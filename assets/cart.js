ccd73f722187441b0ad403970126813a6f8ccbec
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2025-03-22 17:01:48 +0100

    ex1 - maj dynamique du drawer

--- assets/cart.js
+++ assets/cart.js
@@ -182,7 +182,8 @@ class CartItems extends HTMLElement {
 
           this.getSectionsToRender().forEach((section) => {
             const elementToReplace =
-              document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+              document.getElementById(section.id).querySelector(section.selector) ||
+              document.getElementById(section.id);
             elementToReplace.innerHTML = this.getSectionInnerHTML(
               parsedState.sections[section.section],
               section.selector
@@ -213,6 +214,16 @@ class CartItems extends HTMLElement {
         });
 
         CartPerformance.measureFromEvent(`${eventTarget}:user-action`, event);
+
+        // Create a new custom event when the cart is updated
+        const cartUpdateEvent = new CustomEvent('cart_update', {
+          detail: {
+            event: 'cart_update',
+          },
+        });
+
+        // Dispatch the custom event
+        document.dispatchEvent(cartUpdateEvent);
 
         publish(PUB_SUB_EVENTS.cartUpdate, { source: 'cart-items', cartData: parsedState, variantId: variantId });
       })
@@ -284,8 +295,9 @@ if (!customElements.get('cart-note')) {
           'input',
           debounce((event) => {
             const body = JSON.stringify({ note: event.target.value });
-            fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } })
-              .then(() => CartPerformance.measureFromEvent('note-update:user-action', event));
+            fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } }).then(() =>
+              CartPerformance.measureFromEvent('note-update:user-action', event)
+            );
           }, ON_CHANGE_DEBOUNCE_TIMER)
         );
       }
